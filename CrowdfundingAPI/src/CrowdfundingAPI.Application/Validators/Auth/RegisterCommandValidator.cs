6744390636c21b03e4d144de746ecc86c3f231bf
omarmorad <omorad5362@gmail.com>
2025-09-21 18:54:52 +0300

    feat(auth): add validation for register command fields

---
+++ CrowdfundingAPI/src/CrowdfundingAPI.Application/Validators/Auth/RegisterCommandValidator.cs
@@ -0,0 +1,35 @@
+using CrowdfundingAPI.Application.Commands.Auth;
+using FluentValidation;
+
+namespace CrowdfundingAPI.Application.Validators.Auth;
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required")
+            .MaximumLength(100).WithMessage("First name must not exceed 100 characters");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required")
+            .MaximumLength(100).WithMessage("Last name must not exceed 100 characters");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format")
+            .MaximumLength(255).WithMessage("Email must not exceed 255 characters");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long")
+            .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)").WithMessage("Password must contain at least one lowercase letter, one uppercase letter, and one digit");
+
+        RuleFor(x => x.ConfirmPassword)
+            .NotEmpty().WithMessage("Password confirmation is required")
+            .Equal(x => x.Password).WithMessage("Passwords do not match");
+
+        RuleFor(x => x.Bio)
+            .MaximumLength(1000).WithMessage("Bio must not exceed 1000 characters");
+    }
+}
