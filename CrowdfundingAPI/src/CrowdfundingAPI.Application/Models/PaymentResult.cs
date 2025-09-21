a2f36d19e8e74d96ed320caca7d0e3febbe925ab
omarmorad <omorad5362@gmail.com>
2025-09-21 18:53:52 +0300

    feat: Introduce PaymentResult model to encapsulate payment results

---
+++ CrowdfundingAPI/src/CrowdfundingAPI.Application/Models/PaymentResult.cs
@@ -0,0 +1,8 @@
+namespace CrowdfundingAPI.Application.Models;
+
+public class PaymentResult
+{
+    public bool IsSuccess { get; set; }
+    public string TransactionId { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+}
